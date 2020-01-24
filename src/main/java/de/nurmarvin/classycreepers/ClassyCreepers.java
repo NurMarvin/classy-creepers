8012b9b017416798e2072ffa84884b3c31b8b3e3
Marvin Witt <nurmarvin@protonmail.com>
2020-01-24 22:41:31 +0100

    Initial 1.14 version commit

---
+++ src/main/java/de/nurmarvin/classycreepers/ClassyCreepers.java
@@ -0,0 +1,10 @@
+package de.nurmarvin.classycreepers;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ClassyCreepers implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Classy Creepers Mod initialized!");
+	}
+}
